f7cf006149f7a91329783ea9de41c178c36f2423
thepackour <thepackour5610@gmail.com>
2026-06-27 17:57:16 +0900

    대기 큐 레포지토리 완성

---
+++ src/main/java/com/mesh/hello/domain/matching/repository/InMemoryMatchingQueueRepository.java
@@ -0,0 +1,57 @@
+package com.mesh.hello.domain.matching.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+@Repository
+public class InMemoryMatchingQueueRepository implements MatchingQueueRepository {
+    private final Queue<String> helperQueue = new ConcurrentLinkedQueue<>();
+    private final Queue<String> helpeeQueue = new ConcurrentLinkedQueue<>();
+
+    @Override
+    public void pushHelper(String helperSessionId) {
+        if (!helperQueue.contains(helperSessionId)) {
+            helperQueue.add(helperSessionId);
+        }
+    }
+
+    @Override
+    public Optional<String> popWaitingHelper() {
+        return Optional.ofNullable(helperQueue.poll());
+    }
+
+    @Override
+    public void removeHelper(String helperSessionId) {
+        helperQueue.remove(helperSessionId);
+    }
+
+    @Override
+    public Integer getWaitingHelperCount() {
+        return helperQueue.size();
+    }
+
+    @Override
+    public void pushHelpee(String helpeeSessionId) {
+        if (helpeeQueue.contains(helpeeSessionId)) {
+            helpeeQueue.add(helpeeSessionId);
+        }
+    }
+
+    @Override
+    public Optional<String> popWaitingHelpee() {
+        return Optional.ofNullable(helpeeQueue.poll());
+    }
+
+    @Override
+    public void removeHelpee(String helpeeSessionId) {
+        helpeeQueue.remove(helpeeSessionId);
+    }
+
+    @Override
+    public Integer getWaitingHelpeeCount() {
+        return helpeeQueue.size();
+    }
+}
---
+++ src/main/java/com/mesh/hello/domain/matching/repository/MatchingQueueRepository.java
@@ -0,0 +1,15 @@
+package com.mesh.hello.domain.matching.repository;
+
+import java.util.Optional;
+
+public interface MatchingQueueRepository {
+    void pushHelper(String helperSessionId);
+    Optional<String> popWaitingHelper();
+    void removeHelper(String helperSessionId);
+    Integer getWaitingHelperCount();
+
+    void pushHelpee(String helpeeSessionId);
+    Optional<String> popWaitingHelpee();
+    void removeHelpee(String helpeeSessionId);
+    Integer getWaitingHelpeeCount();
+}
