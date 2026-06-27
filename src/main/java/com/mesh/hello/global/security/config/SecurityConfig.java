f3eec1b7e9ce3d5a004768d55e031ba19a22f102
thepackour <thepackour5610@gmail.com>
2026-06-28 03:36:14 +0900

    SecurityConfig CORS 설정

--- src/main/java/com/mesh/hello/global/security/config/SecurityConfig.java
+++ src/main/java/com/mesh/hello/global/security/config/SecurityConfig.java
@@ -94,7 +94,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://hello.sublumen.xyz"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
